f92391aee3753fc5c773d1515f27fa86e0bdb595
Rifat0001 <mahmudrifat764@gmail.com>
2023-11-19 04:10:29 +0600

    error handled

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { chats } = require("./backend/data/data")
 const dotenv = require("dotenv");
 const userRoutes = require('./backend/routes/userRoutes')
+const { notFound, errorHandler } = require("./backend/middleware/errorMiddleware")
 dotenv.config();
 const connectDB = require("./backend/config/db");
 const PORT = process.env.PORT || 5000;
@@ -30,5 +31,8 @@ app.get("/api/chat/:id", (req, res) => {
     const singleChat = chats.find((c) => c._id === req.params.id);
     res.send(singleChat)
 })
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
